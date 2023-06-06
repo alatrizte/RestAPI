37ca95614bd9425bc2f62e1f870a48db53c0bed4
alatrizte <133011925+alatrizte@users.noreply.github.com>
2023-06-06 10:58:06 +0200

    variables.php

--- src/variables.php
+++ src/variables.php
@@ -1,3 +1,3 @@
 <?php 
-$nombreDB = 'almacen';
-$tabla = 'Productos';
+$nombreDB = 'nombre_base_datos'; // Nombre de la base de datos a la que nos referenciamos.
+$tabla = 'nombre_tabla'; // Nombre de la tabla de la que extraemos los datos.
